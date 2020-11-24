3aa183293b68b85e8db758c74dbecbeafe6d391c
jamesvrooney <jamesvrooney@hotmail.com>
2020-11-24 17:15:53 +0000

    Adding web data model. Closes #2

---
+++ src/main/java/com/rooney/james/msscbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,14 @@
+package com.rooney.james.msscbeerservice.web.model;
+
+public enum BeerStyle {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
