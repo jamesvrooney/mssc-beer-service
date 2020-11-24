0888047d84337b45d1ad85fc71685c958fbe3bc5
jamesvrooney <jamesvrooney@hotmail.com>
2020-11-24 16:12:00 +0000

    Initial commit. This closes #1.

---
+++ src/main/java/com/rooney/james/msscbeerservice/JamesBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rooney.james.msscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamesBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamesBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rooney/james/msscbeerservice/JamesBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rooney.james.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamesBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
