d0345c76cb0c58b6d2ff35808f72b056fd64fb50
jamesvrooney <jamesvrooney@hotmail.com>
2020-11-25 04:36:13 +0000

    Adding Beer entity. Closes #5

--- src/main/java/com/rooney/james/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/rooney/james/msscbeerservice/web/controller/BeerController.java
@@ -1,26 +1,43 @@
 package com.rooney.james.msscbeerservice.web.controller;
 
 import com.rooney.james.msscbeerservice.web.model.BeerDto;
+import com.rooney.james.msscbeerservice.web.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
+@Slf4j
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId) {
+        log.info("Getting a beer with id: {}", beerId);
+
+        BeerDto beer = BeerDto.builder()
+                .beerName("Guinness")
+                .beerStyle(BeerStyle.ALE)
+                .version(1)
+                .price(new BigDecimal(3.45))
+                .build();
+
         // TODO: implement
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        return new ResponseEntity<>(beer, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@RequestBody BeerDto newBeer) {
         // TODO: implement
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + UUID.randomUUID());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
